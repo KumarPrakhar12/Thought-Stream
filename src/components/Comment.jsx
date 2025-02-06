69b49826f7cd92e6801f3e0f4dd63a0e4d68586b
KumarPrakhar12 <115941991+KumarPrakhar12@users.noreply.github.com>
2025-02-06 11:58:03 +0530

    added manual comment

---
+++ src/components/Comment.jsx
@@ -0,0 +1,22 @@
+import Image from "./Image"
+
+const Comment = () => {
+  return (
+    <div className="p-4 bg-slate-50 rounded-xl mb-8">
+      <div className="flex items-center gap-4">
+        <Image src="userImg.jpeg" className="w-10 h-10 rounded-full object-cover"
+            w="40"></Image>
+            <span className="font-medium">John Kumar</span>
+            <span className="text-sm text-gray-500">
+          2 days ago
+        </span>
+      </div>
+      <div className="mt-4">
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nihil excepturi magnam error, placeat veniam esse.</p>
+      </div>
+      
+    </div>
+  )
+}
+
+export default Comment
